1e3da5a040a5e85c193bea381f07f70d21436457
VioMrqs <violette.marquis@gmail.com>
2021-12-06 15:17:16 +0100

    database ok

---
+++ db/migrate/20211206140709_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.belongs_to :user, index: true
+      t.belongs_to :idiom, index: true
+      t.timestamps
+    end
+  end
+end
