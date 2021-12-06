ab06e0d129a7f704ed4962ec937993dc2bce7879
Luucas51 <oudartlucas51@gmail.com>
2021-12-06 16:42:58 +0100

    Add view (no css) for index idioms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: "idioms#index"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :idioms
+  resources :users
+  resources :comments
+  resources :team, only: [:show]
+  resources :faq, only: [:show]
 end
---
+++ test/controllers/idioms_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class IdiomsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
