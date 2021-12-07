20291fe7578ed01fa2ff85d71fb853e37653e3b0
Luucas51 <oudartlucas51@gmail.com>
2021-12-07 10:30:35 +0100

    Add show for user and install active storage for user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
-            
+    
+
+    has_one_attached :avatar
     has_many :idioms
     has_many :comments
     has_many :likes
