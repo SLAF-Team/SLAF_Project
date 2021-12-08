fc6ab412b6e5b0d24b850fef9a23b9af7e9e5443
VioMrqs <violette.marquis@gmail.com>
2021-12-08 17:19:25 +0100

    navbar browse ok, footer ok but pagy not working anymore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_085733) do
+ActiveRecord::Schema.define(version: 2021_12_08_144452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,15 @@ ActiveRecord::Schema.define(version: 2021_12_07_085733) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "unlikes", force: :cascade do |t|
+    t.bigint "idiom_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idiom_id"], name: "index_unlikes_on_idiom_id"
+    t.index ["user_id"], name: "index_unlikes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -84,4 +93,6 @@ ActiveRecord::Schema.define(version: 2021_12_07_085733) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "unlikes", "idioms"
+  add_foreign_key "unlikes", "users"
 end
