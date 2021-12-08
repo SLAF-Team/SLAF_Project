d4a17adbfe39dfd07b33ad7aece66054d1bde8d1
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-12-08 09:47:01 +0100

    basic admin connexions

---
+++ test/controllers/admin/idiom_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::IdiomControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/admins/dashboard_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admins::DashboardControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
