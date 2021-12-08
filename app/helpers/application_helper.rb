ac37639966ced34771dde3ff06b11a9721f878b3
VioMrqs <violette.marquis@gmail.com>
2021-12-08 18:39:31 +0100

    fixed temporaly footer & navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,11 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+  
     include Pagy::Frontend
+
+  def letter
+    @letters = *('A'..'Z')
+  end
+
 end
