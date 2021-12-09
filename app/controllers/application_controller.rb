ac03cf9440e4dbfb86930517cd4fe53c343a4db2
SuperOrteg <asseorteg@gmail.com>
2021-12-09 11:01:36 +0100

    Fix form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :error, :warning
   before_action :letter
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pagy::Backend
 
   def letter
     @letters = *('A'..'Z')
   end
-  
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :alias])
+  end
 end
