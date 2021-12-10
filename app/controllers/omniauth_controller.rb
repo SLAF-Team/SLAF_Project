a21d9b2b73a94ac00293b5ab4dfc0a665eb04250
mrprst <prost.mathias@gmail.com>
2021-12-10 16:07:22 +0100

    changing redirect route to social login bis

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -3,20 +3,20 @@ class OmniauthController < ApplicationController
   def facebook
   @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
-      sign_in_and_redirect @user
+      sign_in_and_redirect idioms_path
     else
       flash[:error] = 'Connexion avec Facebook impossible. Veuillez créer un compte ou essayer plus tard.'
-      redirect_to idioms_path
+      redirect_to new_user_registration_url
     end
   end
 
   def google_oauth2
   @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
-      sign_in_and_redirect @user
+      sign_in_and_redirect idioms_path
     else
       flash[:error] = 'Connexion avec Google impossible. Veuillez créer un compte ou réessayer plus tard.'
-      redirect_to idioms_path
+      redirect_to new_user_registration_url
     end
     Rails.logger.info(@user.errors.inspect)
   end
