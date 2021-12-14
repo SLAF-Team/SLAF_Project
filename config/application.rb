453e60d00639bf38eec384939548f6244fa8ca0e
Luucas51 <oudartlucas51@gmail.com>
2021-12-14 16:27:09 +0100

    resolve bug heroku2

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 module SLAFProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
