4a081658589a22bf2ebb4f513ce41e0ac58b07f3
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-12-19 17:01:06 +0100

    with development adress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://syndicatdelutteantifranglais.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://slaf-dev.herokuapp.com/' }
 
 end
