11a146bbaf04608efbb6a029439898856d258b5b
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-12-19 17:31:55 +0100

    removing test line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
